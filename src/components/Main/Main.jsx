c3b17794bed5624b4c7c122a47894726e2fc811b
Bastien Roque <roquembastien@gmail.com>
2026-02-14 11:58:41 +0700

    Corrigir push anterior

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,6 @@ import EditProfile from "./popup/EditProfile/EditProfile.jsx";
 import EditAvatar from "./popup/EditAvatar/EditAvatar.jsx";
 import Card from "../Card/Card.jsx";
 import CurrentUserContext from "../../contexts/CurrentUserContext.js";
-import Popup from "./Popup.jsx";
 
 function Main({ onOpenPopup, cards, onCardLike, onCardDelete }) {
   const currentUser = useContext(CurrentUserContext);
